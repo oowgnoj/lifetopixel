08df13c2a1541396b07cd99e39a30f356a0c4ad7
jongwoo <jongwoo@atommerce.com>
2020-09-22 12:41:09 +0900

    edit environmental variable

--- src/lib/common/index.ts
+++ src/lib/common/index.ts
@@ -1,5 +1,4 @@
-console.log(process.env);
 export const DOMAIN_API =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_DEV_API_URL
-    : process.env.REACT_APP_PROD_API_URL;
+  process.env.REACT_APP_NODE_ENV === "production"
+    ? process.env.REACT_APP_PROD_API_URL
+    : process.env.REACT_APP_DEV_API_URL;
