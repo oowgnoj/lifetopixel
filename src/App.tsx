0fd704762decbd7ea68b980beac66517c73df26b
jongwoo <jongwoo@atommerce.com>
2020-09-26 17:23:31 +0900

    add normalize.css to reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "normalize.css";
 import { UserProvider } from "context/authContext";
 import PrivateRoute from "lib/privateRoute";
 import Index from "./views";
